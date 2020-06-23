652b72880a48db5b410b37d9d7712499373c2c9a
Cindy <cindyzhang977@gmail.com>
2020-06-23 19:28:13 +0000

    change app.js to render servlet text Hello Packaged BEANS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+
   fetch("/api/v1/data")
       .then((response) => response.text())
       .then((text) => {
         console.log("Here is the text from servlet: ", text);
+        setText(text);
       });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     {text}
     </div>
   );
 }
