2ccedb1b175e0fcb0c62b4d68a28b78da5246ef3
Cindy <cindyzhang977@gmail.com>
2020-06-24 21:15:56 +0000

    address PR

--- frontend/src/map/Map.js
+++ frontend/src/map/Map.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Map.css';
 
 function Map({destinations}) {
 
   return (
     <div className="map-container">
-      <iframe width="600" 
-              height="450" 
-              frameBorder="0"
-              title="trip-map"
-              src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyBbZSSvn85LLfo6F5uF1G7VawuvingacM8&origin=Eiffel+Tower&destination=Museum+d'Orsay&waypoints=Louvre|Arc+De+Triomphe" 
-              allowFullScreen
+      <iframe 
+        className="map"
+        frameBorder="0"
+        title="trip-map"
+        src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyBbZSSvn85LLfo6F5uF1G7VawuvingacM8&origin=Eiffel+Tower&destination=Museum+d'Orsay&waypoints=Louvre|Arc+De+Triomphe" 
+        allowFullScreen
       >
       </iframe>
     </div>
