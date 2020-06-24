feb1ecff1bcadc3c9d36f1719e755430f3523e49
Cindy <cindyzhang977@gmail.com>
2020-06-24 17:51:45 +0000

    hard code route, add map css file

--- frontend/src/map/Map.js
+++ frontend/src/map/Map.js
@@ -1,16 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Map() {
+function Map({destinations}) {
 
   return (
-    <iframe width="600" 
-            height="450" 
-            frameBorder="0"
-            title="trip-map"
-            src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyBbZSSvn85LLfo6F5uF1G7VawuvingacM8&origin=Oslo+Norway&destination=Telemark+Norway" 
-            allowFullScreen
-    >
-    </iframe>
+    <div className="map-container">
+      <iframe width="600" 
+              height="450" 
+              frameBorder="0"
+              title="trip-map"
+              src="https://www.google.com/maps/embed/v1/directions?key=AIzaSyBbZSSvn85LLfo6F5uF1G7VawuvingacM8&origin=Eiffel+Tower&destination=Museum+d'Orsay&waypoints=Louvre|Arc+De+Triomphe" 
+              allowFullScreen
+      >
+      </iframe>
+    </div>
   )
 }
 
