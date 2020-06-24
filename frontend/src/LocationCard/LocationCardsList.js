ca31ee571218b50b4b909252861a330f3aa5da0d
Cindy <cindyzhang977@gmail.com>
2020-06-24 21:09:03 +0000

    rename data.js

--- frontend/src/LocationCard/LocationCardsList.js
+++ frontend/src/LocationCard/LocationCardsList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import LocationCard from './LocationCard.js';
-import { Data } from './Data.js';
+import { Data } from './data.js';
 
 function LocationCardsList() {
 
