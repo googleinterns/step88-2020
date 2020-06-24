50db1ac04c8fdfdd957a4fc5e9fbc82640f2dd90
RCAVelez <chve38@gmail.com>
2020-06-24 10:24:07 -0500

    Make ImageComponent

---
+++ frontend/src/ImageComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class ImageComponent extends React.Component {
+  render() {
+    return (
+      <div className="image-container">
+        <img src="" alt=""/>
+      </div>
+    );
+  }
+}
+
+export default ImageComponent
