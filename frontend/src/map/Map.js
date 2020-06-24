538de78b590b36a275415903a4b18ae2a47b8c99
Cindy <cindyzhang977@gmail.com>
2020-06-24 17:11:39 +0000

    add map component

---
+++ frontend/src/map/Map.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function Map() {
+
+  return (
+    <iframe width="600" 
+            height="450" 
+            frameBorder="0"
+            title="trip-map"
+            src="https://www.google.com/maps/embed/v1/directions0?key=AIzaSyBbZSSvn85LLfo6F5uF1G7VawuvingacM8&origin=Oslo+Norway&destination=Telemark+Norway&output=svembed" 
+            allowFullScreen
+    >
+    </iframe>
+  )
+}
+
+export default Map;
