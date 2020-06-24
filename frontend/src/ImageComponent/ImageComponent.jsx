edc7387aa32df35b15a6b4a41bb83038a8332955
RCAVelez <chve38@gmail.com>
2020-06-24 13:17:38 -0500

    Create Image Component

---
+++ frontend/src/ImageComponent/ImageComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function ImageComponent() {
+    return (
+      <div className="image-container">
+        <img src="" alt=""/>
+      </div>
+    )
+}
+
+export default ImageComponent
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ImageComponent from './ImageComponent/ImageComponent'
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ImageComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
