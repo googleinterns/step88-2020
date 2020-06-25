c94c222ba756ca4c8961e713558fc6c351b7fd23
Zoe Barth <zoeb.barth@gmail.com>
2020-06-25 22:59:50 +0000

    finish search component

--- frontend/src/search/Search.js
+++ frontend/src/search/Search.js
@@ -1,17 +1,28 @@
 import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import React from 'react';
+import './Search.css';
+
 
 /** 
- * Creates basic search bar.
+ * Creates Search component with search bar.
  */
 function Search() {
   return (
-    <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="secondary">Search</Button>
-    </Form>
+    <div className="search-container">
+      <div className="where-to">
+        <h1>Where to?</h1>
+      </div>
+      <div className="form">
+        <Form inline>
+          <FormControl type="text" className="mr-sm-2" />
+          <Button variant="secondary"> <FontAwesomeIcon icon={faSearch} className="optimized-icon" />  </Button>
+        </Form>
+      </div>
+    </div>
   )
 }
 
