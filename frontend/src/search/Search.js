02ffa78e62361fa3bc7c57ed96f8f4edbb2e4c35
Zoe Barth <40405806+zoebarth@users.noreply.github.com>
2020-06-25 17:15:55 -0500

    Fix nitpicks

--- frontend/src/search/Search.js
+++ frontend/src/search/Search.js
@@ -4,16 +4,14 @@ import FormControl from 'react-bootstrap/FormControl';
 import React from 'react';
 
 /** 
-  Creates basic search bar.
+ * Creates basic search bar.
  */
 function Search() {
   return (
-    <div>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="secondary">Search</Button>
-      </Form>
-    </div>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="secondary">Search</Button>
+    </Form>
   )
 }
 
