84e0214ee7f0b37df2f5852b9e486c9596470d86
Zoe Barth <zoeb.barth@gmail.com>
2020-06-25 20:40:34 +0000

    add basic search bar component

---
+++ frontend/src/search/Search.js
@@ -0,0 +1,20 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import React from 'react';
+
+/** 
+  Creates basic search bar.
+ */
+function Search() {
+  return (
+    <div>
+     <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="secondary">Search</Button>
+    </Form>
+    </div>
+  )
+}
+
+export default Search;
