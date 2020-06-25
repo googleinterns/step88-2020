d6580db80e094f48bf2d2820d2c98d62bafeb804
Cindy <cindyzhang977@gmail.com>
2020-06-25 23:18:19 +0000

    isLoggedIn prop, handleSave func, remove render

--- frontend/src/route/SaveButton.js
+++ frontend/src/route/SaveButton.js
@@ -4,8 +4,9 @@ import Button from 'react-bootstrap/Button';
 /**
  * Render button to save trip. If trip is saved, rendered disabled button with "Saved" text.
  * @param {boolean|undefined} saved status of whether trip is saved.
+ * @param {boolean|undefined} isLoggedIn status of whether user is logged in to Google account. 
  */
-function SaveButton({saved}) {
+function SaveButton({saved, isLoggedIn}) {
   const [isSaved, setIsSaved] = useState(saved);
 
   if (isSaved) {
@@ -14,8 +15,12 @@ function SaveButton({saved}) {
     );
   }
 
+  handleSave() {
+    setIsSaved(true);
+  }
+
   return (
-    <Button variant="primary" onClick={() => setIsSaved(true)}>Save Trip</Button>
+    <Button variant="primary" onClick={handleSave}>Save Trip</Button>
   );
 }
 
