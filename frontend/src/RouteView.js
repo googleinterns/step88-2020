10706962b3cd2e62ffeedf250389fa1988bc7f0f
Cindy <cindyzhang977@gmail.com>
2020-06-26 21:50:56 +0000

    add components, adjust some styles

--- frontend/src/RouteView.js
+++ frontend/src/RouteView.js
@@ -1,10 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import styles from './RouteView.module.css';
+
+import Map from './map/Map.js';
+import Route from './route/Route.js';
+import OptimizeButton from './route/OptimizeButton.js';
+import SaveButton from './route/SaveButton.js';
+import TripName from './trip-name/TripName.js';
+
+import { MOCK_DATA } from './route/mockData.js';
 
 function RouteView() {
  return(
-   <>
-   </>
+   <Container>
+    <Row>
+      <TripName />
+    </Row>
+    <Row>
+      <Col>
+        <Row className={styles.routeContainer}>
+          <Route places={MOCK_DATA} />
+        </Row>
+        <Row>
+          <OptimizeButton />
+        </Row>
+      </Col>
+      <Col>
+        <Row>
+          <Map mode='directions' />
+        </Row>
+        <Row>
+          <SaveButton />
+        </Row>
+      </Col>
+    </Row>
+   </Container>
  )
 }
 
