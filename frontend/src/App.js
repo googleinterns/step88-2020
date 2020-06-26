fefd2500c9cb08bd678f5e787b898f7f70383761
Cindy <cindyzhang977@gmail.com>
2020-06-26 22:23:44 +0000

    trip modularized

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 
+import T from './trip-name/TripName.js'
+
 function App() {
   const [text, setText] = useState('');
 
@@ -16,6 +18,7 @@ function App() {
         Click Me!
       </Button>
       <div>{text}</div>
+      <T />
     </div>
   );
 }
