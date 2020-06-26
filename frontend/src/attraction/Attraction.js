bd7822d359e13f66df366d0345ecfeff14e045fc
RCAVelez <chve38@gmail.com>
2020-06-26 12:43:38 -0500

    Add back Attraction.js

---
+++ frontend/src/attraction/Attraction.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Attraction.css';
+
+/**
+ * Attraction contains the image of the attraction.
+ * props.imageUrl contains source of image.
+ */
+function Attraction(props) {
+  return (
+    <div className='attraction-container'>
+      <img src={props.imageUrl} />
+    </div>
+  );
+}
+
+export default Attraction;
