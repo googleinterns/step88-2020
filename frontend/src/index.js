c01615a93f36c6c81baecb89303ffcd74ddc1f42
RCAVelez <chve38@gmail.com>
2020-06-29 09:35:49 -0500

    Changed to module css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import NavButton from './navigation/NavButton';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <NavButton link={'/route'} label={'Show Route'} />
   </React.StrictMode>,
   document.getElementById('root')
 );
