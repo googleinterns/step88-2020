ee94509651d4568c1c8105a49ea4d1a1a8eecf96
Zoe Barth <zoe.barth@gmail.com>
2020-06-29 17:37:14 +0000

    run prettier

--- frontend/src/SearchView.js
+++ frontend/src/SearchView.js
@@ -3,21 +3,21 @@ import SavedTrips from './saved-trips/SavedTrips.js';
 import Search from './search/Search.js';
 import styles from './SearchView.module.css';
 
-/** 
+/**
  * Creates Search View component, with saved trips rendered if user is logged in.
  * @param {boolean} loggedIn - if the user is logged in.
  */
-function SearchView({loggedIn}) {
-  const trips = ["London", "Paris"];
-  
+function SearchView({ loggedIn }) {
+  const trips = ['London', 'Paris'];
+
   let savedTrips;
   if (loggedIn) {
-    savedTrips = <SavedTrips trips={trips}/>;
+    savedTrips = <SavedTrips trips={trips} />;
   }
 
   return (
     <div className={styles.container}>
-      <Search/>
+      <Search />
       {savedTrips}
     </div>
   );
