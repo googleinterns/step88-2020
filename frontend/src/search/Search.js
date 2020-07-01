557ae2263b8cd9e2639fc8f0f1c2cc7c082e8885
Zoe Barth <zoe.barth@gmail.com>
2020-07-01 16:22:11 +0000

    add search bar link

--- frontend/src/search/Search.js
+++ frontend/src/search/Search.js
@@ -3,31 +3,41 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styles from './Search.module.css';
+import { useHistory } from 'react-router-dom';
 
 /**
  * Creates Search component with search bar.
  */
 function Search() {
-  const [name, setName] = useState("");
+  const [name, setName] = useState('');
+  let history = useHistory();
 
   return (
     <div className={styles.searchContainer}>
       <div className={styles.whereTo}>
         <h1>Where to?</h1>
       </div>
-      <Form inline className={styles.form}>
-        <FormControl type="text" className="mr-sm-2 " value={name}
-          onChange={e => setName(e.target.value)}/>
-        <Button variant="secondary" type="submit" href="/explore">
+      <Form
+        inline
+        className={styles.form}
+        onSubmit={() => {
+          history.push(`/explore?search=${name}`);
+        }}
+      >
+        <FormControl
+          type="text"
+          className="mr-sm-2 "
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Button variant="secondary" type="submit">
           <FontAwesomeIcon icon={faSearch} className="optimized-icon" />
         </Button>
       </Form>
     </div>
   );
 }
-
-
 
 export default Search;
