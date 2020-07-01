4b9bfd5e380b40aaae4fc9e9117a6315423d1e85
Zoe Barth <zoe.barth@gmail.com>
2020-07-01 18:59:32 +0000

    add placeholder navigation bar

--- frontend/src/navbar/Navbar.js
+++ frontend/src/navbar/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
+import styles from './Navbar.module.css';
 
 /**
  * Creates Navbar component with login button.
  */
 function Navbar({ loggedIn, onLoginChange }) {
   return (
-    <Nav className="justify-content-end">
+    <Nav className={styles.container}>
+      <Nav.Item className={styles.a}>
+        <Nav.Link href="/">
+        Back
+        </Nav.Link>
+      </Nav.Item>
       <Nav.Item>
-        <Nav.Link onClick={() => onLoginChange(!loggedIn)} href="">
+        <Nav.Link onClick={() => onLoginChange(!loggedIn)} href="" className={styles.b}>
           {loggedIn ? 'Sign Out' : 'Sign In'}
         </Nav.Link>
       </Nav.Item>
