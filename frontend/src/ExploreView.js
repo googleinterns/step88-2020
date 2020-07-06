46a3d4754ac7c5a5dd2f4d66b85812bb5152cfab
RCAVelez <chve38@gmail.com>
2020-07-06 10:44:47 -0500

    url parameters converted to dict with getQueryParameters()

--- frontend/src/ExploreView.js
+++ frontend/src/ExploreView.js
@@ -3,12 +3,17 @@ import styles from './ExploreView.module.css';
 import AttractionsPanel from './attractions-panel/AttractionsPanel';
 import NavButton from './navigation/NavButton';
 import Map from './map/Map';
+import { useLocation} from "react-router-dom";
+
 
 /**
  * Explore view with attraction selection and map
  * @param {list} images list image urls
  */
 function Explore({ images }) {
+  const location = useLocation();
+  const queryParameters = getQueryParameters(location.search);
+  console.log(queryParameters.search);
   return (
     <div className={styles.exploreContainer}>
       <div className={styles.attractionsSection}>
@@ -20,6 +25,24 @@ function Explore({ images }) {
       </div>
     </div>
   );
+
+  /**
+   * Extract the url parameters and convert to dictionary
+   * @param {string} query string of url parameters
+   * @return {object} key value pair of url parameters
+   */
+  function getQueryParameters(query) {
+    const params = query.split('?')[1];
+    const attributes = params.split('&');
+    let paramsDict = [];
+
+    for (const param of attributes) {
+      const key = param.split('=')[0];
+      const value = param.split('=')[1];
+      paramsDict[key] = value;
+    }
+    return paramsDict;
+  }
 }
 
 export default Explore;
