5e4bfcb36cb300cf3ded057a0085c0287df8c667
Cindy <cindyzhang977@gmail.com>
2020-07-06 22:42:55 +0000

    add space

--- backend/src/main/java/com/google/sps/servlets/OptimizeServlet.java
+++ backend/src/main/java/com/google/sps/servlets/OptimizeServlet.java
@@ -38,8 +38,8 @@ public class OptimizeServlet extends HttpServlet {
       DistanceMatrixApiRequest req = DistanceMatrixApi.newRequest(context); 
       DistanceMatrix result = req.origins(origins).destinations(destinations).await();
 
-      for(int i = 0; i < origins.length; i++) {
-        for(int j = 0; j < destinations.length; j++) {
+      for (int i = 0; i < origins.length; i++) {
+        for (int j = 0; j < destinations.length; j++) {
           int distance = result.rows[i].elements[j].distance.inMeters;
         }
       } 
