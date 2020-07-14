707990effd7a6b233ee80db62cb52e06a94fcbb9
RCAVelez <chve38@gmail.com>
2020-07-14 14:56:27 -0500

    Placeholder, autocomplete, beans title

--- frontend/src/search/Search.js
+++ frontend/src/search/Search.js
@@ -1,50 +1,65 @@
-import Button from 'react-bootstrap/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
 import React, { useState } from 'react';
 import styles from './Search.module.css';
 import { useHistory } from 'react-router-dom';
+import usePlacesAutocomplete from 'use-places-autocomplete';
+import { Typeahead } from 'react-bootstrap-typeahead';
 
 /**
  * Creates Search component with search bar.
  */
 function Search() {
-  const [query, setQuery] = useState('');
   const history = useHistory();
+  const [options, setOptions] = useState([]);
+  const {
+    suggestions: { status, data },
+    setValue,
+  } = usePlacesAutocomplete();
+
+  const handleInput = (e) => {
+    // Update the keyword of the input element
+    setValue(e);
+  };
+
+  const renderSuggestions = () => {
+    setOptions(
+      data.map((suggestion) => {
+        const {
+          structured_formatting: {main_text},
+        } = suggestion;
+        return main_text;
+      })
+    );
+  };
 
   return (
     <div className={styles.searchContainer}>
       <div className={styles.whereTo}>
         <h1 className={styles.text}>
-          <span className={styles.blue}>W</span>
-          <span className={styles.red}>h</span>
-          <span className={styles.yellow}>e</span>
-          <span className={styles.green}>r</span>
-          <span className={styles.blue}>e</span>
-          <span className={styles.grey}> to?</span>
+          <span className={styles.blue}>B.</span>
+          <span className={styles.red}>E.</span>
+          <span className={styles.yellow}>A.</span>
+          <span className={styles.green}>N.</span>
+          <span className={styles.blue}>S.</span>
         </h1>
       </div>
-      <Form
-        inline
-        className={styles.form}
-        onSubmit={() => {
-          if (query !== '') {
-            history.push(`/explore?search=${query}`);
+      <Typeahead
+        type="text"
+        id="basic-typeahead-single"
+        className={styles.searchBar}
+        onInputChange={(text) => {
+          handleInput(text);
+          status === 'OK' && renderSuggestions();
+        }}
+        onChange={(text) => {
+          if (text !== '') {
+            history.push(`/explore?search=${text[0]}`);
           }
         }}
-      >
-        <FormControl
-          type="text"
-          className={styles.searchBar}
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        />
-        <Button variant="secondary" type="submit">
-          <FontAwesomeIcon icon={faSearch} className="optimized-icon" />
-        </Button>
-      </Form>
+        options={options}
+        placeholder="Where to?"
+      />
     </div>
   );
 }
