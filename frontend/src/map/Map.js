adc17d02e5c61e1a3a209e99b79e70ab832dbb55
Cindy <cindyzhang977@gmail.com>
2020-07-16 17:10:01 +0000

    tried

--- frontend/src/map/Map.js
+++ frontend/src/map/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Map as GoogleMap, GoogleApiWrapper } from 'google-maps-react';
 import styles from './Map.module.css';
 const MAPS_API_KEY = 'AIzaSyDD_xK2HDMKPmDrsHndH5SAK9Jl-k5rHdg';
@@ -12,12 +12,15 @@ const MAPS_EMBED_URL = 'https://www.google.com/maps/embed/v1/directions';
  */
 
 function Map({ attractions, mode, centerLocation, google, onReady, view }) {
+  const [lastOpenInfoWindow, setLastOpenInfoWindow] = useState(null);
+  
   const onPinsReady = (mapProps, map) => {
     onReady(google, map);
     for (const attraction of attractions) {
+      const id = `${attraction.lat}-${attraction.lng}`;
       const infowindow = new google.maps.InfoWindow({
         content: `
-          <div>
+          <div id=${id}>
             <h4 class=${styles.infoWindowName}>${attraction.name}</h4>
             <div class=${styles.infoWindowDescription}>Short description of attraction if desired</div>
             <div>
@@ -36,8 +39,19 @@ function Map({ attractions, mode, centerLocation, google, onReady, view }) {
         title: attraction.name,
       });
       //TODO: clean up listeners -> potential memory leak
-      marker.addListener('click', () => {
+      marker.addListener('click', (elem) => {
         infowindow.open(map, marker);
+        console.log("last open")
+        console.log(lastOpenInfoWindow)
+        console.log("boolean value")
+        console.log(lastOpenInfoWindow && document.getElementById(lastOpenInfoWindow).close())
+        console.log("get elem by id")
+        console.log(document.getElementById(lastOpenInfoWindow))
+        lastOpenInfoWindow && document.getElementbyId(lastOpenInfoWindow).close();
+        const newId = `${elem.latLng.lat()}-${elem.latLng.lng()}`
+        console.log("new id")
+        console.log(newId)
+        setLastOpenInfoWindow(newId);
       });
     }
   };
