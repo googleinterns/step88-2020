f5b325aa8ec4007c07e501f1b75c7eeb6aafb209
RCAVelez <chve38@gmail.com>
2020-07-17 12:42:51 -0500

    util functions for fetch requests to create and read user

---
+++ frontend/src/UserCRUD.js
@@ -0,0 +1,46 @@
+export const createUser = (email) => {
+  fetch(`/api/v1/createUser?email=${email}`)
+    .then((response) => {
+      if (response.status !== 200) {
+        return {};
+      }
+      try {
+        return JSON.parse(response);
+      } catch {
+        return {};
+      }
+    })
+    .catch((err) => {
+      return {};
+    });
+};
+
+export const readUser = (email) => {
+  fetch(`/api/v1/readUser?email=${email}`)
+    .then((response) => {
+      if (response.status !== 200) {
+        return {};
+      }
+      response.json().then((response) => {
+        return response;
+      });
+    })
+    .catch((err) => {
+      return {};
+    });
+};
+
+export const updateUser = (email, trips) => {
+  fetch(`/api/v1/updateUser?email=${email}&trips=${trips}`)
+    .then((response) => {
+      if (response.status !== 200) {
+        return {};
+      }
+      response.json().then((response) => {
+        return response;
+      });
+    })
+    .catch((err) => {
+      return {};
+    });
+};
