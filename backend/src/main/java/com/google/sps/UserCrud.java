d22e3909dbfb2ecb3d758318c83f3e317f946ed3
RCAVelez <chve38@gmail.com>
2020-07-17 12:37:48 -0500

    user crud class with create and read

---
+++ backend/src/main/java/com/google/sps/UserCrud.java
@@ -0,0 +1,34 @@
+package com.google.sps;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.gson.*;
+
+public class UserCrud {
+  DatastoreService datastore;
+
+  public UserCrud(DatastoreService datastore) {
+    this.datastore = datastore;
+  }
+
+  public void createUser(String email) {
+    Entity userEntity = new Entity("User");
+    userEntity.setProperty("email", email);
+    userEntity.setProperty("trips", "[]");
+    datastore.put(userEntity);
+  }
+
+  public Entity readUser(String email) {
+    Query query = new Query("User");
+    PreparedQuery results = datastore.prepare(query);
+    for (Entity userEntity : results.asIterable()) {
+      String entityEmail = userEntity.getProperty("email").toString();
+      if (entityEmail.equals(email)) {
+        return userEntity;
+      }
+    }
+    return null;
+  }
+}
