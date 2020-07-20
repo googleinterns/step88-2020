1318ae21ab96bbe11057f6a41fcfc9ea56c66ea2
RCAVelez <chve38@gmail.com>
2020-07-20 12:28:37 -0500

    removed additional paremthesis in each function

--- frontend/src/UserCRUD.js
+++ frontend/src/UserCRUD.js
@@ -2,16 +2,16 @@ export const createUser = async (email) => {
   const response = await fetch(`/api/v1/createUser?email=${email}`);
   const json = await response.json();
   return json;
-});
+};
 
 export const readUser = async (email) => {
   const response = await fetch(`/api/v1/readUser?email=${email}`);
   const json = await response.json();
   return json;
-});
+};
 
 export const updateUser = async (email, trips) => {
   const response = await fetch(`/api/v1/updateUser?email=${email}&trips=${trips}`);
   const json = await response.json();
   return json;
-});
+};
