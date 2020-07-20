d6ed2e0f292351d3109426a08133dc9fff23e6de
RCAVelez <chve38@gmail.com>
2020-07-20 12:25:21 -0500

    util functions return json

--- frontend/src/UserCRUD.js
+++ frontend/src/UserCRUD.js
@@ -1,66 +1,17 @@
 export const createUser = async (email) => {
   const response = await fetch(`/api/v1/createUser?email=${email}`);
   const json = await response.json();
-  console.log(json);
   return json;
 });
 
 export const readUser = async (email) => {
   const response = await fetch(`/api/v1/readUser?email=${email}`);
   const json = await response.json();
-  console.log(json);
   return json;
 });
 
 export const updateUser = async (email, trips) => {
   const response = await fetch(`/api/v1/updateUser?email=${email}&trips=${trips}`);
   const json = await response.json();
-  console.log(json);
   return json;
 });
-
-
-// export const createUser = (email) => {
-//   fetch(`/api/v1/createUser?email=${email}`)
-//     .then((response) => {
-//       if (response.status !== 200) {
-//         return {};
-//       }
-//       response.json().then((response) => {
-//         return response;
-//       });
-//     })
-//     .catch((err) => {
-//       return {};
-//     });
-// };
-//
-// export const readUser = (email) => {
-//   fetch(`/api/v1/readUser?email=${email}`)
-//     .then((response) => {
-//       if (response.status !== 200) {
-//         return {};
-//       }
-//       response.json().then((response) => {
-//         return response;
-//       });
-//     })
-//     .catch((err) => {
-//       return {};
-//     });
-// };
-//
-// export const updateUser = (email, trips) => {
-//   fetch(`/api/v1/updateUser?email=${email}&trips=${trips}`)
-//     .then((response) => {
-//       if (response.status !== 200) {
-//         return {};
-//       }
-//       response.json().then((response) => {
-//         return response;
-//       });
-//     })
-//     .catch((err) => {
-//       return {};
-//     });
-// };
