96ddc14e63142e3a7a80c7c8da1906683b9e72e6
RCAVelez <chve38@gmail.com>
2020-07-22 12:49:18 -0500

    updated create user servlet

--- backend/src/main/java/com/google/sps/servlets/createUserServlet.java
+++ backend/src/main/java/com/google/sps/servlets/createUserServlet.java
@@ -14,11 +14,12 @@
 
 package com.google.sps.servlets;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.sps.UserCrud;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,19 +28,22 @@ import javax.servlet.http.HttpServletResponse;
 /** Servlet that creates a new user */
 @WebServlet("/api/v1/createUser")
 public class createUserServlet extends HttpServlet {
-  DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-  UserCrud userCrud = new UserCrud(datastore);
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String email = request.getParameter("email");
-    if (email == "" || email == null || userCrud.readEntity("email", email, "User") != null) {
+
+    if (email == "" || email == null || UserCrud.readEntity("email", email, "User") != null) {
       throw new IllegalArgumentException("Email passed is not valid");
     }
 
-    userCrud.createUser(email);
+    Entity userEntity = UserCrud.createUser(email);
+    ArrayList<String> tripIds = (ArrayList<String>) userEntity.getProperty("tripIds");
 
     JsonObject jsonResults = new JsonObject();
+    Gson gson = new Gson();
+    jsonResults.addProperty("email", userEntity.getProperty("email").toString());
+    jsonResults.addProperty("tripIds", gson.toJson(tripIds));
+
     response.setContentType("application/json;");
     response.getWriter().println(jsonResults);
   }
