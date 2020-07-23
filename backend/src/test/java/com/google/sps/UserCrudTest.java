6c56d7e9b4617f2600490d1230d6f0b291f5767c
RCAVelez <chve38@gmail.com>
2020-07-23 12:35:14 -0500

    added dependencies for datastore testing, created sample test

--- backend/src/test/java/com/google/sps/UserCrudTest.java
+++ backend/src/test/java/com/google/sps/UserCrudTest.java
@@ -1,49 +1,51 @@
-// // Copyright 2019 Google LLC
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     https://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
+// Copyright 2019 Google LLC
 //
-// package com.google.sps;
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// import com.google.appengine.api.datastore.DatastoreService;
-// import com.google.appengine.api.datastore.DatastoreServiceFactory;
-// import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
-// import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-// import org.junit.After;
-// import org.junit.Before;
+//     https://www.apache.org/licenses/LICENSE-2.0
 //
-// public class UserCrudTest {
-//   private final LocalServiceTestHelper helper =
-//       new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
-//
-//   private DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-//   private UserCrud userCrud = new UserCrud(datastore);
-//
-//   @Before
-//   public void setUp() {
-//     helper.setUp();
-//   }
-//
-//   @After
-//   public void tearDown() {
-//     helper.tearDown();
-//   }
-//
-//   // @Test
-//   // public void createUserTest() {
-//   //   Entity userEntity = userCrud.createUser("temail@gmail.com");
-//   //   String userEmail = (String) userEntity.getProperty("email");
-//   //   System.out.println(userEmail);
-//   //   // Key key = dataServices.saveTask("daily", "unique_id343434");
-//   //   // assertEquals("unique_id343434", key.getName());
-//   // }
-// }
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class UserCrudTest {
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
+
+  @Before
+  public void setUp() {
+    helper.setUp();
+  }
+
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
+
+  @Test
+  public void createUserTest() {
+    assertEquals(1, 1);
+    //   Entity userEntity = userCrud.createUser("temail@gmail.com");
+    //   String userEmail = (String) userEntity.getProperty("email");
+    //   System.out.println(userEmail);
+    //   // Key key = dataServices.saveTask("daily", "unique_id343434");
+    //   // assertEquals("unique_id343434", key.getName());
+  }
+}
