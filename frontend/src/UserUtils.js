69edaaa1e2f0b3b361a635b6b55e6eb423723ae0
RCAVelez <chve38@gmail.com>
2020-07-23 09:10:22 -0500

    not storing json, directly returning, tripData consts camelCase to UPCASE_SNAKE

--- frontend/src/UserUtils.js
+++ frontend/src/UserUtils.js
@@ -2,12 +2,10 @@ import { fetchJson } from './fetchJson.js';
 
 export const createUser = async (email) => {
   const url = `/api/v1/createUser?email=${email}`;
-  const json = await fetchJson(url);
-  return json;
+  return await fetchJson(url);
 };
 
 export const readUser = async (email) => {
   const url = `/api/v1/readUser?email=${email}`;
-  const json = await fetchJson(url);
-  return json;
+  return await fetchJson(url);
 };
